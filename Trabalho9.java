34cb7b0c26e035cfc348b75e1dd66f16ce8a5fa1
Ramon Mg <114538472+Ramon1754@users.noreply.github.com>
2022-11-28 11:21:07 -0300

    first commit

---
+++ Trabalho9.java
@@ -0,0 +1,21 @@
+package Mod9;
+
+public class Trabalho9 {
+
+   public static void main(String ars[]) {
+
+       Boolean status = true;
+
+       Character tipoSanguineo = 'A';
+
+       Integer idade = 25;
+
+       Long cpf = Long.valueOf(14555802291l);
+
+       System.out.println("Status:" + status);
+       System.out.println("Tipo Sanguineo:" + tipoSanguineo);
+       System.out.println("Idade:" + idade);
+       System.out.println("CPF:" + cpf);
+
+   }
+}
